67864cefd05d19f0998fc75ade2d90aea0fd537b
SukritaEarn <sukrita.ki@ku.th>
2020-09-23 19:10:52 +0700

    use annotation-based

--- src/main/java/atm/AtmUI.java
+++ src/main/java/atm/AtmUI.java
@@ -1,15 +1,18 @@
 package atm;
-
 import java.io.IOException;
 import java.util.Scanner;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
    A text-based simulation of an automatic teller machine.
  */
+@Component
 public class AtmUI {
 
 	private ATM atm;
 
+	@Autowired
 	public AtmUI(ATM atm) {
 		this.atm = atm;
 	}
